417afa6b202026005d9cccd1dab78d4025530edb
Justin <sobek7@gmail.com>
2016-05-21 15:48:40 -0400

    Added basic node class

---
+++ Graph/Graph/Node.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Graph
+{
+	class Node<T>
+	{
+		#region constructors
+		public Node(T data, List<Node<T>> previousNodes = null, List<Node<T>> nextNodes = null)
+		{
+			Data = data;
+			PreviousNodes = previousNodes;
+			NextNodes = nextNodes;
+		}
+
+
+		#endregion
+		public T Data { get; set; }
+		public List<Node<T>> PreviousNodes { get; set; }
+		public List<Node<T>> NextNodes { get; set; }
+	}
+}
