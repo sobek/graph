58a1f10dc7acf4ae999cc4224feb6d09b7c63091
Justin <sobek7@gmail.com>
2016-05-21 22:43:42 -0400

    New test. Added first add case and test method.

--- Graph/Graph/Graph.cs
+++ Graph/Graph/Graph.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,26 +7,61 @@ using System.Threading.Tasks;
 
 namespace Graph
 {
-	public class Graph<T> where T : new()
+	public class Graph<T, U> : IEnumerable<T> where T : Node<U>
 	{
-		public Graph() { }
+		private List<T> nodes;
+
+		IEnumerator<T> IEnumerable<T>.GetEnumerator() =>
+			nodes.GetEnumerator();
+
+		public IEnumerator GetEnumerator() =>
+			nodes.GetEnumerator();
+
+
+		public Graph()
+		{
+			nodes = new List<T>();
+		}
 		/// <summary>
 		/// Adds a node to the end of a graph. This method will
 		/// add a node to all nodes defined as the current 'end' 
 		/// by determining whether they have any 'next' nodes.
 		/// </summary>
-		/// <param name="graph"></param>
 		/// <param name="node"></param>
-		/// <returns></returns>
-		public static Graph<T> Add(Graph<T> graph, Node<T> node)
+		/// <returns>Returns true if node was successfully added.</returns>
+		public bool Add(Node<U> node)
 		{
-			throw new NotImplementedException();
+			var endNodes = nodes.Where(t => (t.NextNodes?.Count ?? 0) == 0);
+			if (!(nodes.Count > 0))
+			{
+				try
+				{
+					nodes.Add(node as T);
+				}
+				catch (Exception) //TODO Specify exception types.
+				{
+					return false;
+				}
+				return true;
+			}
+
+			return !true;
+			//throw new NotImplementedException();
+
 		}
 
-		public Graph<T> Remove(Graph<T> graph, Node<T> node)
-		{
-			throw new NotImplementedException();
-		}
+
+		///// <summary>
+		///// Removes the first instance of a given node. If node occurs at the same level 
+		///// in different paths, it will only be removed from the first path.
+		///// </summary>
+		///// <param name="graph"></param>
+		///// <param name="node"></param>
+		///// <returns></returns>
+		//public Graph<T> Remove(Graph<T> graph, Node<T> node)
+		//{
+		//	throw new NotImplementedException();
+		//}
 	}
 
 
---
+++ Graph/UnitTestProject1/GraphUnitTests.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using Graph;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+	[TestClass]
+	public class GraphUnitTests
+	{
+		private readonly Graph<Node<int>, int> graph = new Graph<Node<int>, int>();
+		private readonly Node<int> node = new Node<int>(42);
+
+		[TestMethod]
+		public void Graph_AddNode()
+		{
+			graph.Add(node);
+
+			Assert.IsTrue(graph.Contains(node));
+		}
+	}
+}
